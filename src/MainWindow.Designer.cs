6eb561b2be539a2b8b1bd7a56ba26524a03e947b
Dasutein <dustine_2008@hotmail.com>
2019-03-22 11:04:33 +0800

    Initial commit

---
+++ src/MainWindow.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace BrightnessControl
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(190, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "BRIGHTNESS_VALUE";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(366, 56);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "GET_UPDATED_BRIGHTNESS_VALUE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(398, 132);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(365, 56);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "GET_LAST_BRIGHTNESS_VALUE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(26, 204);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(178, 34);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(26, 254);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(365, 56);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "SET_CUSTOM_BRIGHTNESS_VALUE";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Brightness Controller";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ src/MainWindow.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Runtime.InteropServices;
+using System.Management; //add dll to reference
+using Microsoft.Win32;
+using System.Threading;
+using System.Diagnostics;
+
+namespace BrightnessControl
+{
+    public partial class MainWindow : Form
+    {
+
+        int LAST_BRIGHTNESS_VALUE;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            //SetBrightness(20);
+
+            var brightnessValue = GetBrightness();
+            label1.Text = brightnessValue.ToString();
+
+            // Get the current brightness then save to integer 
+            LAST_BRIGHTNESS_VALUE = GetBrightness();
+        }
+
+        static void SetBrightness(byte targetBrightness)
+        {
+            ManagementScope scope = new ManagementScope("root\\WMI");
+            SelectQuery query = new SelectQuery("WmiMonitorBrightnessMethods");
+            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query))
+            {
+                using (ManagementObjectCollection objectCollection = searcher.Get())
+                {
+                    foreach (ManagementObject mObj in objectCollection)
+                    {
+                        mObj.InvokeMethod("WmiSetBrightness",
+                            new Object[] { UInt32.MaxValue, targetBrightness });
+                        break;
+                    }
+                }
+            }
+        }
+
+        static int GetBrightness()
+        {
+            ManagementScope scope = new ManagementScope("root\\WMI");
+            SelectQuery query = new SelectQuery("WmiMonitorBrightness");
+            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query))
+            {
+                using (ManagementObjectCollection objectCollection = searcher.Get())
+                {
+                    foreach (ManagementObject mObj in objectCollection)
+                    {
+                        var br_obj = mObj.Properties["CurrentBrightness"].Value;
+                        Debug.WriteLine("BRIGHTNESS_LEVEL" + br_obj);
+                        int br = 0;
+                        int.TryParse(br_obj + "", out br);
+                        return br;
+                        
+                    }
+                }
+            }
+            return 0;
+
+        }
+
+        public static byte[] ConvertInt32ToByteArray(Int32 I32)
+        {
+            return BitConverter.GetBytes(I32);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            byte[] bytes = ConvertInt32ToByteArray(LAST_BRIGHTNESS_VALUE);
+            byte bytetest = bytes[0];
+            SetBrightness(bytetest);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            var brightnessValue = GetBrightness();
+            label1.Text = brightnessValue.ToString();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            byte[] bytes = ConvertInt32ToByteArray(LAST_BRIGHTNESS_VALUE);
+            byte bytetest = bytes[0];
+            SetBrightness(bytetest);
+
+            var brightnessValue = GetBrightness();
+            label1.Text = brightnessValue.ToString();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            int NEW_BRIGHTNESS_VALUE = int.Parse(textBox1.Text);
+            byte[] bytes = ConvertInt32ToByteArray(NEW_BRIGHTNESS_VALUE);
+            byte bytetest = bytes[0];
+            SetBrightness(bytetest);
+
+            var brightnessValue = GetBrightness();
+            label1.Text = brightnessValue.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
